bf308bd8b4e9215bee290853bce2699b9ccec599
Saphie <safihanadi28@gmail.com>
2020-06-18 20:49:21 +0300

    feat: image url valide for http/s

--- models/cube.js
+++ models/cube.js
@@ -11,4 +11,9 @@ const cubeSchema = new mongoose.Schema({
   accessories: [{ type: mongoose.Types.ObjectId, ref: "Accessories" }],
   creatorId: String
 });
+
+cubeSchema.path('imageUrl').validate(function(url){
+  return url.startsWith('http') || url.startsWith('https')
+}, 'Image url is invalid')
+
 module.exports = mongoose.model("Cube", cubeSchema);
